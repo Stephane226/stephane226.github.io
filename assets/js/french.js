225a05a85b4daa588061bf7c8edbf4d246b8cbb3
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2020-04-19 02:03:42 +0300

    fr up

--- assets/js/french.js
+++ assets/js/french.js
@@ -1,53 +1,58 @@
-function Showeventt() {
-    var knowmeid = '<div class="zer" > <br><div class="moi"> <table style="text-align:center;width:100%;margin-top:-30px;margin-bottom:-30px;"> <tr> <td style="width:0%">111 </td> <td class="zerb1xx" id="zedkass"><h2 >  i am A lot Of Kind surpri Del </h2>or a silver fern job search work visa. You can also  <br> I have a Passion fo Creating Clean, Beautiful, Interactive, Minimalistic,  <br>Select one of the options below to apply for a working holiday visa,  <br> and Functional Code.<br>a silver fern job search work visa. You can also  <br> I have a Passion fo Creating Clean, Beautiful, Interactive, Minimalistic,  <br>Select one of the options below to apply for a working holiday visa, <br>a silver fern job search work visa. You can also  <br> I have a Passion fo Creating Clean, Beautiful, Interactive, Minimalistic,  <br>Select one of the options below to apply for a working holiday visa, <br> <img src="assets/img/moimeme.png" height="100px" width="100px"><br></td><td style="width:13%"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="#er"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup"> </a></td> </tr> </table>. </div><br></div>'
-    document.getElementById("knowmeid").innerHTML = knowmeid
-    document.getElementById('inter').style.backgroundColor = 'rgb(236, 178, 236)'
-}
 
-function showeventa() {
-    var knowmeid = '<div class="zera" id="xb"> <br><h3 style="color:rgb(241, 239, 100);margin-bottom:25px; padding-right:70px;" id="srvc">I provide these services </h3> <br><div class="container"><div class="row" style="text-align:center;padding-left:24px"><div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;"><img src="assets/img/translate.jpeg" height="130px" class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title" style="color:black">Translate with me</h5><p style="font-size:15px;color:black"> İ will translate your blogs or website using a profesionaly style of İ will translate your blogs  or website using aprofesionaly style of a good way for the business of all world tools go on demop here.i work in coorporate conditions </p> <br> <a href="#" class="btn btn-primary">İNTEREST </a> </div> </div></div>  <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;"><img src="assets/img/seo.jpg" height="130px" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">SEO </h5><p style="color:black; font-size:15px"> looking to quickly add Bootstrap to your project? Use BootstrapCDN, provided for free by the folks at StackPath. Using a package manager or need to download the source files also all the trafic on google is wery hudge</p> <br> <a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="padding-bottom:15px "><div class="card" style="width: 19rem;"><img src="assets/img/develop.jpeg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">web design</h5><p style="color:black;font-size:15px"> Curious which components explicitly require jQuery, our JS, and Popper.js? Click the show components link below. If you’re at all unsure about the general page structure, keep reading </p> <br><a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> </div></div><div class="ese"><a href="#er" ;style="text-align:right"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup1" style="text-align:right;"></a></div><br></div>';
-    document.getElementById("knowmeid").innerHTML = knowmeid;
+//show "about me" on div which have inter as id
+function Showeventt() {
+    var knowmeid = '<div class="zer" > <br><div class="moi"> <table style="text-align:center;width:100%;margin-top:-30px;margin-bottom:-30px;"> <tr> <td style="width:">111 </td> <td class="zerb1xx" id="zedkass"><h2 >  Who Am I ? </h2>My name is Stephane. I am from Burkina Faso, and I live in Turkey.<br>I am all about professional results to handle long-term collaborations. <br>With more than Three years of experience; I work on translation, websites <br>SEO and Web Design. I design with a focus on visual and interaction design. <br>I build portfolios and websites based on a deep understanding of customers’ needs.<br> I have worked for many projects about Web Design, SEO and Translation and ; <br>accomplish customers’ needs has always been my first priority. <br> <br><img src="assets/img/moimeme.png" height="100px" width="100px"><br></td><td style="width:13%"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="#er"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup"> </a></td> </tr> </table>. </div><br></div>'
+    document.getElementById("knowmeid").innerHTML = knowmeid
+    document.getElementById('inter').style.backgroundColor = 'rgb(236, 178, 236)'  
+}
+//show "servis" on div which have inter as id
+function showeventa(){
+   var knowmeid = '<div class="zera" id="xb"> <br><h3 style="color:rgb(241, 239, 100);margin-bottom:25px;" id="srvc">My Services </h3> <br><div class="container" id="con"><div class="row" style="text-align:center;"><div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/translate.jpeg" height="130px" class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title" style="color:black">Traduisez avec moi</h5><p style="font-size:14px;color:black"> With me, do your online professional translation, you do not need to look for a translation office in order to get professional document translation. <strong>I am available 24/7 </strong>to help you with any document translation questions you might have. Your translation will be done by myself. In order to deliver the best professional translation service possible, i have implemented <strong>rigorous quality work procedures</strong> as a part of every document translation process. </p> <br> <a href="#" class="btn btn-primary">İNTEREST </a> </div> </div></div>  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/seo.jpg" height="130px" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">SEO </h5><p style="color:black; font-size:14px">  More traffic and content writing. <br> As you gain more visibility, the number of your visitors will increase. <strong>Content writing </strong>is the most important part of <strong>search engine optimization.</strong>  If you don’t have a good content, no one will not understand clearly what is about your business, and that means no one will find you within the rankings . In that way i will be your French,English and Turkish SEO web copy content writer. <br> </p> <br> <a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:14px "><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/develop.jpeg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">web design</h5><p style="color:black;font-size:15px"> If you need <strong>custom designed portfolio </strong>or website,i am here to start working on your project. Your website is quite often the first contact you will have with your clients. Its an ideal tool for artists, photographers, designers, and other creatives. Your website will influence the way you are perceived by your audience, so it is important that it is carefully made.</p> <br><a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> </div></div><div class="ese"><a href="#er" ;style="text-align:right"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup1" style="text-align:right;"></a></div><br></div>';
+    document.getElementById("knowmeid").innerHTML = knowmeid; 
     document.getElementById('inter').style.backgroundColor = 'yellow'
 }
-
-function showeventb() {
-    var knowmeid = '<div class="zerb" id="xc"> <br> <div class="zerb1"> <table style="text-align:center;width:100%;margin-top:-30px;margin-bottom:-30px;"> <tr> <td style="width:10%">111 </td> <td class="zerb1x" id="zedkas"><h2> A lot Of Kind surpri Del </h2>or a silver fern job search work visa. You can also  <br> I have a Passion fo Creating Clean, Beautiful, Interactive, Minimalistic,  <br>Select one of the options below to apply for a working holiday visa,  <br> and Functional Code.<br>a silver fern job search work visa. You can also  <br> I have a Passion fo Creating Clean, Beautiful, Interactive, Minimalistic,  <br>Select one of the options below to apply for a working holiday visa, <br>a silver fern job search work visa. You can also  <br> I have a Passion fo Creating Clean, Beautiful, Interactive, Minimalistic,  <br>Select one of the options below to apply for a working holiday visa, <br><br><br><br><br><br><br><br><br><br></td><td style="width:10%"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="#er" style="padding-bottom:5px"><img src="assets/img/up.png" width="50px" height="50px" class="goup"></a>  </td> </tr> </table></div> <br></div>';
+//show "blog" on div which have inter as id
+function showeventb(){
+   var knowmeid = '<div class="zerb" id="xc"> <br> <div class="zerb1"> <table style="text-align:center;width:100%;margin-top:-30px;margin-bottom:-30px;"> <tr> <td style="width:10%">111 </td> <td class="zerb1x" id="zedkas"><h5> What is On Page SEO? </h5><br> On-page SEO refers to everything on the page itself that affects your SEO. <br>On-page SEO includes everything that exists on the page, both in published <br>and  HTMLformat.The three major types of on-page SEO factors are content,<br> architecture, and HTML <br> <br> <h5>What is Off-Page SEO? <h5/> <br> <br> Off-page SEO refers to everything that affects a page’s <br>SEO that isn’t necessarily tied down to the page in question; <br>off-page SEO factors exist outside of the page. <br>They’re harder to optimize because they aren’t under the page <br>owner’s control, but best practices still go a long way. Off-page SEO <br>factors include trustworthiness, link building, and social engagement. <br><br> <h5>WHY SHOULD WE BE IN THE FIRST PAGE OF GOOGLE SEARCHES?</h5><br> <br>Less than 10% of visitors clicks the ?\"NEXT PAGE\" <br>The search page of Google is considered to be the holy grail of <br>SERPs (Search Engine Results Pages). It’s because people these days<br> don’t even bother to check the second page of Google.<br> If they don’t find their relevant topics on the first page,<br> they simply adjust their search terms. <br>So a better Google <br>ranking is a must for promoting your business. Being on the first page <br>of Google has its many advantages like more<br>traffic to your website, new customers, and increased engagement.<br>When your business website doesn’t appear on the first page <br>of Google, you have a lesser chance of earning a greater return <br>on investment. Being on the first page of Google gives you the<br>maximum online visibility and more sales.<br>To beat the competition, your business has to be easy to find <br>on the Internet. And only a better Google <br>ranking can help you with that.<br><br><br><br><br><br><br><br></td><td style="width:10%"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="#er" style="padding-bottom:5px"><img src="up.png" width="50px" height="50px" class="goup"></a>  </td> </tr> </table></div> <br></div>';
     document.getElementById("knowmeid").innerHTML = knowmeid;
     document.getElementById('inter').style.backgroundColor = 'rgb(0, 253, 0)'
 }
 
-function turncolorto() {
-    document.getElementById('interbord0').style.backgroundColor = 'yellow'
-    document.getElementById('interbord2').style.backgroundColor = 'yellow'
-    document.getElementById('er').style.color = 'rgb(255, 255, 0)'
+function turncolorto(){
+                       
+           document.getElementById('interbord0').style.backgroundColor = 'yellow' 
+           document.getElementById('interbord2').style.backgroundColor = 'yellow' 
+
+           document.getElementById('er').style.color = 'rgb(255, 255, 0)'  
+                   }
+function checkcolorto(){
+            document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
+            document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
+            document.getElementById('er').style.color = 'rgb(253, 253, 177)'  
 }
 
-function checkcolorto() {
-    document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)'
-    document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-    document.getElementById('er').style.color = 'rgb(253, 253, 177)'
-}
-
-function turncolorto1() {
-    document.getElementById('interbord0').style.backgroundColor = 'rgb(63, 196, 22)'
-    document.getElementById('interbord2').style.backgroundColor = 'rgb(63, 196, 22)'
-    document.getElementById('er1').style.color = 'rgb(0, 253, 0)'
-}
-
-function checkcolorto1() {
-    document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)'
-    document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-    document.getElementById('er1').style.color = 'rgb(100, 158, 100)'
-}
-
-function turncolorto2() {
-    document.getElementById('interbord0').style.backgroundColor = 'rgb(236, 178, 236)'
-    document.getElementById('interbord2').style.backgroundColor = 'rgb(236, 178, 236)'
-    document.getElementById('er2').style.color = 'rgb(243, 7, 243)'
-}
-
-function checkcolorto2() {
-    document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)'
-    document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-    document.getElementById('er2').style.color = 'rgb(241, 168, 241)'
-}
+function turncolorto1(){ 
+                       
+                       document.getElementById('interbord0').style.backgroundColor = 'rgb(63, 196, 22)' 
+                       document.getElementById('interbord2').style.backgroundColor = 'rgb(63, 196, 22)'
+                       document.getElementById('er1').style.color = 'rgb(0, 253, 0)'  
+                     
+            }
+            function checkcolorto1(){
+                        document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
+                        document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)' 
+                        document.getElementById('er1').style.color = 'rgb(100, 158, 100)'
+            }
+            
+function turncolorto2(){
+                       
+                       document.getElementById('interbord0').style.backgroundColor = 'rgb(236, 178, 236)' 
+                       document.getElementById('interbord2').style.backgroundColor = 'rgb(236, 178, 236)' 
+                       document.getElementById('er2').style.color = 'rgb(243, 7, 243)' 
+                     
+            }
+            function checkcolorto2(){
+                        document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
+                        document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
+                        document.getElementById('er2').style.color = 'rgb(241, 168, 241)' 
+            }
