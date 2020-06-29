0cb55e0c604e39d04ccdea171c43ece9cbb93fb6
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2020-06-29 20:36:57 +0300

    g dev

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,56 +3,57 @@
 function Showeventt() {
     var knowmeid = '<div class="zer" > <br><div class="moi"> <table style="text-align:center;width:100%;margin-top:-35px;margin-bottom:-20px;"> <tr> <td style="width:5%"></td> <td class="zerb1xx" id="zedkass"><h4>  Who Am I ? </h4><div class="aboutme"><p>My name is Stephane. I am from Burkina Faso, and I live in Turkey.I am all about professional results to handle long-term collaborations. With more than Three years of experience; I work on translation, websites SEO and Web Design. I design with a focus on visual and interaction design.I build portfolios and websites based on a deep understanding of customers’ needs.I have worked for many projects about Web Design, SEO and Translation and ;accomplish customers’ needs has always been my first priority. <br> <br><img src="assets/img/moimeme.png" height="100px" width="100px"><br></td></p></div><td style="width:5%"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="#er"><img src="assets/img/up.png" alt="go up"width="35px" height="35px" class="goup"> </a></td> </tr> </table> </div><br></div>'
     document.getElementById("knowmeid").innerHTML = knowmeid;
-    document.getElementById('inter').style.backgroundColor = 'rgb(236, 178, 236)'  
+    document.getElementById('inter').style.backgroundColor = 'whitesmoke'  
 }
 //show "servis" on div which have inter as id
 function showeventa(){
-   var knowmeid = '<div class="zera" id="xb"> <br><h3 style="color:rgb(241, 239, 100);margin-bottom:25px;" id="srvc">My Services </h3> <br><div class="container" id="con"><div class="row" style="text-align:center;"><div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/translate.jpeg" height="130px" class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title" style="color:black">Translate With Me l</h5><p style="font-size:14px;color:black"> With me, do your online professional translation, you do not need to look for a translation office in order to get professional document translation. <strong>I am available 24/7 </strong>to help you with any document translation questions you might have. Your translation will be done by myself. In order to deliver the best professional translation service possible, i have implemented <strong>rigorous quality work procedures</strong> as a part of every document translation process. </p> <br> <a href="#" class="btn btn-primary">İNTEREST </a> </div> </div></div>  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/seo.jpg" height="130px" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">SEO </h5><p style="color:black; font-size:14px">  More traffic and content writing. <br> As you gain more visibility, the number of your visitors will increase. <strong>Content writing </strong>is the most important part of <strong>search engine optimization.</strong>  If you don’t have a good content, no one will not understand clearly what is about your business, and that means no one will find you within the rankings . In that way i will be your French,English and Turkish SEO web copy content writer. <br> </p> <br> <a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:14px "><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/develop.jpeg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">web design</h5><p style="color:black;font-size:15px"> If you need <strong>custom designed portfolio </strong>or website,i am here to start working on your project. Your website is quite often the first contact you will have with your clients. Its an ideal tool for artists, photographers, designers, and other creatives. Your website will influence the way you are perceived by your audience, so it is important that it is carefully made.</p> <br><a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> </div></div><div class="ese"><a href="#er" ;style="text-align:right"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup1" style="text-align:right;"></a></div><br></div>';
+   var knowmeid = '<div class="zera" id="xb"> <br><h3 style="color:black;margin-bottom:25px;" id="srvc">. </h3> <br><div class="container" id="con"><div class="row" style="text-align:center;"><div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/translate.jpeg" height="130px" class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title" style="color:black">Translate With Me l</h5><p style="font-size:14px;color:black"> With me, do your online professional translation, you do not need to look for a translation office in order to get professional document translation. <strong>I am available 24/7 </strong>to help you with any document translation questions you might have. Your translation will be done by myself. In order to deliver the best professional translation service possible, i have implemented <strong>rigorous quality work procedures</strong> as a part of every document translation process. </p> <br> <a href="#" class="btn btn-primary">İNTEREST </a> </div> </div></div>  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/seo.jpg" height="130px" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">SEO </h5><p style="color:black; font-size:14px">  More traffic and content writing. <br> As you gain more visibility, the number of your visitors will increase. <strong>Content writing </strong>is the most important part of <strong>search engine optimization.</strong>  If you don’t have a good content, no one will not understand clearly what is about your business, and that means no one will find you within the rankings . In that way i will be your French,English and Turkish SEO web copy content writer. <br> </p> <br> <a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:14px "><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/develop.jpeg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">web design</h5><p style="color:black;font-size:15px"> If you need <strong>custom designed portfolio </strong>or website,i am here to start working on your project. Your website is quite often the first contact you will have with your clients. Its an ideal tool for artists, photographers, designers, and other creatives. Your website will influence the way you are perceived by your audience, so it is important that it is carefully made.</p> <br><a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> </div></div><div class="ese"><a href="#er" ;style="text-align:right"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup1" style="text-align:right;"></a></div><br></div>';
     document.getElementById("knowmeid").innerHTML = knowmeid; 
-    document.getElementById('inter').style.backgroundColor = 'yellow'
+    document.getElementById('inter').style.backgroundColor = 'whitesmoke' 
 }
 //show "blog" on div which have inter as id
 function showeventb(){
    var knowmeid = '<div class="zerb" id="xc"> <br> <div class="zerb1"> <div class="shadow p-3 mb-5 bg-white rounded" style="width:90%;margin-right:5%;margin-left:5%"><div style="padding-left:30px;padding-right:30px;text-align:center"> <div class="shadow p-3 mb-5 bg-white rounded" id="zedkas" style="background-color:#F8F8F8">What Is On Page SEO? <br>On-page SEO refers to everything on the page itself that affects your SEO. On-page SEO includes everything that exists on the page, both in published and HTMLformat.The three major types of on-page SEO factors are content,architecture, and HTML <br> <br>What is Off-Page SEO? <br>Off-page SEO refers to everything that affects a page’s SEO that isn’t necessarily tied down to the page in question; off-page SEO factors exist outside of the page. They’re harder to optimize because they aren’t under the page owner’s control, but best practices still go a long way. Off-page SEO factors include trustworthiness, link building, and social engagement. <br><br>WHY SHOULD WE BE IN THE FIRST PAGE OF GOOGLE SEARCHES? <br>Less than 10% of visitors clicks the ?"NEXT PAGE" The search page of Google is considered to be the holy grail of SERPs (Search Engine Results Pages). It’s because people these daysdon’t even bother to check the second page of Google.If they don’t find their relevant topics on the first page,they simply adjust their search terms. So a better Google <br>  ranking is a must for promoting your business. Being on the first page of Google has its many advantages like more traffic to your website, new customers, and increased engagement. When your business website doesn’t appear on the first page  of Google, you have a lesser chance of earning a greater return on investment. Being on the first page of Google gives you themaximum online visibility and more sales.To beat the competition, your business has to be easy to find on the Internet. And only a better Google ranking can help you with that.<br><br><br><br><br><br><br><br><br><br></div></div></div> </div><br></div>';
     document.getElementById("knowmeid").innerHTML = knowmeid;
-    document.getElementById('inter').style.backgroundColor = 'rgb(0, 253, 0)'
+    document.getElementById('inter').style.backgroundColor = 'whitesmoke' 
 }
+
 
 function turncolorto(){
                        
-           document.getElementById('interbord0').style.backgroundColor = 'yellow' 
-           document.getElementById('interbord2').style.backgroundColor = 'yellow' 
+           document.getElementById('interbord0').style.backgroundColor = 'white' 
+           document.getElementById('interbord2').style.backgroundColor = 'white' 
 
-           document.getElementById('er').style.color = 'rgb(255, 255, 0)'  
+           document.getElementById('er').style.color = 'rgb(150, 243, 238)'  
                    }
 function checkcolorto(){
             document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
             document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-            document.getElementById('er').style.color = 'rgb(253, 253, 177)'  
+            document.getElementById('er').style.color = 'rgb(8, 146, 146)'  
 }
 
 function turncolorto1(){ 
                        
-                       document.getElementById('interbord0').style.backgroundColor = 'rgb(63, 196, 22)' 
-                       document.getElementById('interbord2').style.backgroundColor = 'rgb(63, 196, 22)'
-                       document.getElementById('er1').style.color = 'rgb(0, 253, 0)'  
+                       document.getElementById('interbord0').style.backgroundColor = 'white'
+                       document.getElementById('interbord2').style.backgroundColor = 'white'
+                       document.getElementById('er1').style.color = 'rgb(150, 243, 238)'  
                      
             }
             function checkcolorto1(){
                         document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
                         document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)' 
-                        document.getElementById('er1').style.color = 'rgb(100, 158, 100)'
+                        document.getElementById('er1').style.color = 'rgb(8, 146, 146)'
             }
             
 function turncolorto2(){
                        
-                       document.getElementById('interbord0').style.backgroundColor = 'rgb(236, 178, 236)' 
-                       document.getElementById('interbord2').style.backgroundColor = 'rgb(236, 178, 236)' 
-                       document.getElementById('er2').style.color = 'rgb(243, 7, 243)' 
+                       document.getElementById('interbord0').style.backgroundColor = 'white'
+                       document.getElementById('interbord2').style.backgroundColor = 'white' 
+                       document.getElementById('er2').style.color = 'rgb(150, 243, 238)' 
                      
             }
             function checkcolorto2(){
                         document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
                         document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-                        document.getElementById('er2').style.color = 'rgb(241, 168, 241)' 
+                        document.getElementById('er2').style.color = 'rgb(8, 146, 146)' 
             }
--- assets/js/turkish.js
+++ assets/js/turkish.js
@@ -2,56 +2,56 @@
 function Showeventt() {
     var knowmeid = '<div class="zer" > <br><div class="moi"> <table style="text-align:center;width:100%;margin-top:-20px;margin-bottom:-20px;"> <tr> <td style="width:5%"></td> <td class="zerb1xx" id="zedkass"><h4> Kimim ? </h4><div class="aboutme"><p>Benim adım Stephane. Burkina Fasoluyum ve Türkiyede yaşıyorum. Uzun vadeli işbirlikler sürdürmek vastasında tamamen profesyonel sonuçlar sunuyorum. Üç yıldan fazla tecrübemle; Çeviri, SEO ve Web Tasarımı üzerinde çalışıyorum. Görsel ve etkileşim tasarıma odaklanarak tasarlarım. Müşterilerin ihtiyaçlarını derin bir şekilde derin bir şekilde anlamakla  portföyler ve web siteleri oluşturuyorum. Web Tasarım, SEO ve Çeviri ile ilgili birçok proje üzerinde çalıştım ve müşterilerin ihtiyaçlarını karşılamak her zaman benim önceliğim olmuştur.<br> <br><img src="assets/img/moimeme.png" height="100px" width="100px"><br></p></div><td style="width:5%"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><a href="#er"><img src="assets/img/up.png" alt="go up"width="35px" height="35px" class="goup"> </a></td> </tr> </table> </div><br></div>'
     document.getElementById("knowmeid").innerHTML = knowmeid
-    document.getElementById('inter').style.backgroundColor = 'rgb(236, 178, 236)'  
+    document.getElementById('inter').style.backgroundColor = 'whitesmoke'  
 }
 //show "servis" on div which have inter as id
 function showeventa(){
-   var knowmeid = '<div class="zera" id="xb"> <br><h3 style="color:rgb(241, 239, 100);margin-bottom:25px;" id="srvc">Hizmetlerim </h3> <br><div class="container" id="con"><div class="row" style="text-align:center;"><div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/translate.jpeg" height="130px" class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title" style="color:black">Tercüme Benimle Yapın</h5><p style="font-size:14px;color:black"> Benimle profesyonel çevirinizi yapın.Profesyonel belge çevirisi almak için bir çeviri ofisi aramanıza gerek yok. <strong>Herhangi bir doküman çevirisi veya website çivirisi ile ilgili işler konusunda size yardımcı olmak için 7/24 hazırım.</strong>. Tercümenizi kendim yaparım. Mümkün olan en iyi profesyonel çeviri hizmetini sunabilmek için her çeviri adımında titiz ve kaliteli iş prosedürleri uyguluyorum.</p> <br> <a href="#" class="btn btn-primary"> Interest</a> </div> </div></div>  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/seo.jpg" height="130px" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">SEO </h5><p style="color:black; font-size:14px"> Daha fazla trafik ve içerik düzeni. <br><strong> Ne kadar görünürlük kazanırsanız,o kadar ziyaretçilerinizin sayısı artacaktır.</strong> İçerik yazma, arama motoru optimizasyonunun en önemli parçasıdır. İyi bir içeriğiniz (content)yoksa, hiç kimse işletmenizle ilgili ne olduğunu net bir şekilde anlamayacak ve bu, hiç kimsenin sizi google sıralamalarıda bulamayacağı anlamına gelir. Bu şekilde Fransızca, İngilizce ve/veya Türkçe SEO web kopyası içerik yazarı ben olurum. <br> </p> <br> <a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:14px "><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/develop.jpeg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">web Tasarımı</h5><p style="color:black;font-size:15px"> Özel olarak tasarlanmış bir portfolio veya websiteye ihtiyacınız varsa, projeniz üzerinde çalışmaya başlamak için hazırım. <strong>En çok,web sayfanız müşterilerinizle görüşeceğiniz ilk platformdur. </strong>Sanatçılar, fotoğrafçılar, tasarımcılar ve diğer girişimciler için ideal bir araçtır. Web siteniz kitleniz tarafından  modelinizi de kişiliğinizi etkileyecektir, buna dikkat ederek hizmetinizi en iyi şeklinde sunarım.</p> <br><a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> </div></div><div class="ese"><a href="#er" ;style="text-align:right"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup1" style="text-align:right;"></a></div><br></div>';
+   var knowmeid = '<div class="zera" id="xb"> <br><h6 style="color:black;margin-bottom:25px;" id="srvc">.</h6> <br><div class="container" id="con"><div class="row" style="text-align:center;"><div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/translate.jpeg" height="130px" class="card-img-top" alt="..."> <div class="card-body"><h5 class="card-title" style="color:black">Tercüme Benimle Yapın</h5><p style="font-size:14px;color:black"> Benimle profesyonel çevirinizi yapın.Profesyonel belge çevirisi almak için bir çeviri ofisi aramanıza gerek yok. <strong>Herhangi bir doküman çevirisi veya website çivirisi ile ilgili işler konusunda size yardımcı olmak için 7/24 hazırım.</strong>. Tercümenizi kendim yaparım. Mümkün olan en iyi profesyonel çeviri hizmetini sunabilmek için her çeviri adımında titiz ve kaliteli iş prosedürleri uyguluyorum.</p> <br> <a href="#" class="btn btn-primary"> Interest</a> </div> </div></div>  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:15px"><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/seo.jpg" height="130px" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">SEO </h5><p style="color:black; font-size:14px"> Daha fazla trafik ve içerik düzeni. <br><strong> Ne kadar görünürlük kazanırsanız,o kadar ziyaretçilerinizin sayısı artacaktır.</strong> İçerik yazma, arama motoru optimizasyonunun en önemli parçasıdır. İyi bir içeriğiniz (content)yoksa, hiç kimse işletmenizle ilgili ne olduğunu net bir şekilde anlamayacak ve bu, hiç kimsenin sizi google sıralamalarıda bulamayacağı anlamına gelir. Bu şekilde Fransızca, İngilizce ve/veya Türkçe SEO web kopyası içerik yazarı ben olurum. <br> </p> <br> <a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4" style="padding-bottom:14px "><div class="card" style="width: 19rem;background-color:#F8F8F8"><img src="assets/img/develop.jpeg" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title" style="color:black">web Tasarımı</h5><p style="color:black;font-size:15px"> Özel olarak tasarlanmış bir portfolio veya websiteye ihtiyacınız varsa, projeniz üzerinde çalışmaya başlamak için hazırım. <strong>En çok,web sayfanız müşterilerinizle görüşeceğiniz ilk platformdur. </strong>Sanatçılar, fotoğrafçılar, tasarımcılar ve diğer girişimciler için ideal bir araçtır. Web siteniz kitleniz tarafından  modelinizi de kişiliğinizi etkileyecektir, buna dikkat ederek hizmetinizi en iyi şeklinde sunarım.</p> <br><a href="#" class="btn btn-primary">ınterest</a> </div> </div></div> </div></div><div class="ese"><a href="#er" ;style="text-align:right"><img src="assets/img/up.png" alt="go up"width="50px" height="50px" class="goup1" style="text-align:right;"></a></div><br></div>';
     document.getElementById("knowmeid").innerHTML = knowmeid; 
-    document.getElementById('inter').style.backgroundColor = 'yellow'
+    document.getElementById('inter').style.backgroundColor = 'whitesmoke' 
 }
 //show "blog" on div which have inter as an id
 function showeventb(){
    var knowmeid = '<div class="zerb" id="xc"> <br> <div class="zerb1"> <div class="shadow p-3 mb-5 bg-white rounded" style="width:90%;margin-right:5%;margin-left:5%"><div style="padding-left:30px;padding-right:30px;text-align:center"> <div class="shadow p-3 mb-5 bg-white rounded" id="zedkas" style="background-color:#F8F8F8">What Is On Page SEO? <br>On-page SEO refers to everything on the page itself that affects your SEO. On-page SEO includes everything that exists on the page, both in published and HTMLformat.The three major types of on-page SEO factors are content,architecture, and HTML <br> <br>What is Off-Page SEO? <br>Off-page SEO refers to everything that affects a page’s SEO that isn’t necessarily tied down to the page in question; off-page SEO factors exist outside of the page. They’re harder to optimize because they aren’t under the page owner’s control, but best practices still go a long way. Off-page SEO factors include trustworthiness, link building, and social engagement. <br><br>WHY SHOULD WE BE IN THE FIRST PAGE OF GOOGLE SEARCHES? <br>Less than 10% of visitors clicks the ?"NEXT PAGE" The search page of Google is considered to be the holy grail of SERPs (Search Engine Results Pages). It’s because people these daysdon’t even bother to check the second page of Google.If they don’t find their relevant topics on the first page,they simply adjust their search terms. So a better Google <br>  ranking is a must for promoting your business. Being on the first page of Google has its many advantages like more traffic to your website, new customers, and increased engagement. When your business website doesn’t appear on the first page  of Google, you have a lesser chance of earning a greater return on investment. Being on the first page of Google gives you themaximum online visibility and more sales.To beat the competition, your business has to be easy to find on the Internet. And only a better Google ranking can help you with that.<br><br><br><br><br><br><br><br><br><br></div></div></div> </div><br></div>';
     document.getElementById("knowmeid").innerHTML = knowmeid;
-    document.getElementById('inter').style.backgroundColor = 'rgb(0, 253, 0)'
+    document.getElementById('inter').style.backgroundColor = 'whitesmoke' 
 }
 
 function turncolorto(){
                        
-           document.getElementById('interbord0').style.backgroundColor = 'yellow' 
-           document.getElementById('interbord2').style.backgroundColor = 'yellow' 
+    document.getElementById('interbord0').style.backgroundColor = 'white' 
+    document.getElementById('interbord2').style.backgroundColor = 'white' 
 
-           document.getElementById('er').style.color = 'rgb(255, 255, 0)'  
-                   }
+    document.getElementById('er').style.color = 'rgb(150, 243, 238)'  
+            }
 function checkcolorto(){
-            document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
-            document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-            document.getElementById('er').style.color = 'rgb(253, 253, 177)'  
+     document.getElementById('interbord0').style.backgroundColor = "black"
+     document.getElementById('interbord2').style.backgroundColor = "black"
+     document.getElementById('er').style.color = 'rgb(8, 146, 146)'  
 }
 
 function turncolorto1(){ 
-                       
-                       document.getElementById('interbord0').style.backgroundColor = 'rgb(63, 196, 22)' 
-                       document.getElementById('interbord2').style.backgroundColor = 'rgb(63, 196, 22)'
-                       document.getElementById('er1').style.color = 'rgb(0, 253, 0)'  
-                     
-            }
-            function checkcolorto1(){
-                        document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
-                        document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)' 
-                        document.getElementById('er1').style.color = 'rgb(100, 158, 100)'
-            }
-            
+                
+                document.getElementById('interbord0').style.backgroundColor = 'white'
+                document.getElementById('interbord2').style.backgroundColor = 'white'
+                document.getElementById('er1').style.color = 'rgb(150, 243, 238)'  
+              
+     }
+     function checkcolorto1(){
+                 document.getElementById('interbord0').style.backgroundColor = "black"
+                 document.getElementById('interbord2').style.backgroundColor = "black"
+                 document.getElementById('er1').style.color = 'rgb(8, 146, 146)'
+     }
+     
 function turncolorto2(){
-                       
-                       document.getElementById('interbord0').style.backgroundColor = 'rgb(236, 178, 236)' 
-                       document.getElementById('interbord2').style.backgroundColor = 'rgb(236, 178, 236)' 
-                       document.getElementById('er2').style.color = 'rgb(243, 7, 243)' 
-                     
-            }
-            function checkcolorto2(){
-                        document.getElementById('interbord0').style.backgroundColor = 'rgb(30, 218, 202)' 
-                        document.getElementById('interbord2').style.backgroundColor = 'rgb(30, 218, 202)'
-                        document.getElementById('er2').style.color = 'rgb(241, 168, 241)' 
-            }
+                
+                document.getElementById('interbord0').style.backgroundColor = 'white'
+                document.getElementById('interbord2').style.backgroundColor = 'white' 
+                document.getElementById('er2').style.color = 'rgb(150, 243, 238)' 
+              
+     }
+     function checkcolorto2(){
+                 document.getElementById('interbord0').style.backgroundColor = "black"
+                 document.getElementById('interbord2').style.backgroundColor = "black"
+                 document.getElementById('er2').style.color = 'rgb(8, 146, 146)' 
+     }
